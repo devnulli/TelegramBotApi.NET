6e8e4b29e38a9852c30cfa55de1d75e761306743
Michael Schönbauer <michael.schoenbauer@gmx.at>
2019-02-07 16:31:37 +0100

    coding style

--- TelegramBotApi/TelegramBotApi/Converters/UnixToDateTimeConverter.cs
+++ TelegramBotApi/TelegramBotApi/Converters/UnixToDateTimeConverter.cs
@@ -4,11 +4,16 @@ namespace nerderies.TelegramBotApi.Converters
 {
     public class UnixToDateTimeConverter
     {
-        public static DateTime Convert(long date)
+        /// <summary>
+        /// converts a unix timestamp to an actual datetime
+        /// </summary>
+        /// <param name="unixDate">the date in seconds since 1970-01-01 00:00</param>
+        /// <returns>the converted Datetime</returns>
+        public static DateTime Convert(long unixDate)
         {
-            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(date).ToLocalTime();
-            return dtDateTime;
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixDate).ToLocalTime();
+            return dateTime;
         }
     }
 }
